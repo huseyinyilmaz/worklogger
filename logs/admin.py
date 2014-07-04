10986225a840b9e03e88df364741fefcea5adbe3
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2014-07-04 19:53:55 +0300

    Work on datamodel.

--- logs/admin.py
+++ logs/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from logs.models import Job, Log
+
+
+class LogAdmin(admin.ModelAdmin):
+    save_on_top = True
+    save_as = True
+    date_hierarchy = 'day'
+
+    list_display = ('user', 'day', 'start', 'finish', 'job')
+
+    list_filter = ('user', 'job')
+
+
+class LogInline(admin.TabularInline):
+    model = Log
+
+
+class JobAdmin(admin.ModelAdmin):
+    inlines = [LogInline]
+
 # Register your models here.
-admin.site.register(Job)
-admin.site.register(Log)
+admin.site.register(Job, JobAdmin)
+admin.site.register(Log, LogAdmin)
--- logs/models.py
+++ logs/models.py
@@ -9,6 +9,13 @@ class Job(models.Model):
     """
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created_at', 'name')
+
+    def __unicode__(self):
+        return self.name
 
 
 class Log(models.Model):
@@ -17,5 +24,17 @@ class Log(models.Model):
     """
     user = models.ForeignKey(User)
     job = models.ForeignKey(Job)
-    start = models.DateTimeField()
-    finish = models.DateTimeField()
+    day = models.DateField()
+    start = models.TimeField()
+    finish = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['user', '-day', '-start', '-finish']
+
+    def __unicode__(self):
+        return u'Log for %s: %s at %s (%s-%s)' % (self.user,
+                                                  self.job,
+                                                  self.day,
+                                                  self.start,
+                                                  self.finish)
