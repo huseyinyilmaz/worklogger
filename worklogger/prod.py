cf4ae21a996e6942b48119fa0a0e1298c4f1a861
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2017-04-24 21:19:11 -0700

    Add django redis cache backend.

--- worklogger/prod.py
+++ worklogger/prod.py
@@ -3,13 +3,24 @@ from worklogger.base import *  # noqa
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS.extend(['www.yilmazhuseyin.com',
+ALLOWED_HOSTS.extend(['www.yilmazhuseyin.com', # noqa
                       'worklogger.yilmazhuseyin.com'])
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-        'LOCATION': '127.0.0.1:11211',
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
         'KEY_PREFIX': 'worklogger',
     }
 }
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+#         'LOCATION': '127.0.0.1:11211',
+#         'KEY_PREFIX': 'worklogger',
+#     }
+# }
